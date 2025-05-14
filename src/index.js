5e008e7e7571c7723d8dc81c6239851fa143045b
DevanshTomar <tomardevansh83@gmail.com>
2025-05-14 11:55:44 -0700

    utils code setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import connnectDB from './db/index.js'
 dotenv.config({ path: './env' })
 
 connnectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log('ERROR connecting', error)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`SERVER is RUNNIG at port: ${process.env.PORT}`)
+    }) 
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err)
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const reqHandler = (requestHandler) => {
+    (req, res, next) => {
+        promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }
+//     catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
